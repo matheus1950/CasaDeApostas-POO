2d83a37e0f28d9810eb95e39b6e19fa433f73009
YuriRoberto <yurirobertocm@gmail.com>
2024-08-13 21:41:57 -0300

    tela de boas-vindas

---
+++ src/betTeste/Janela.java
@@ -0,0 +1,16 @@
+package betTeste;
+
+import javax.swing.JPanel;
+
+public class Janela extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Janela() {
+
+	}
+
+}
---
+++ src/betTeste/Main.java
@@ -0,0 +1,11 @@
+package betTeste;
+
+public class Main {
+
+	public static void main(String[] args) {
+		User user = new User();
+		
+		System.out.println(user);
+	}
+
+}
